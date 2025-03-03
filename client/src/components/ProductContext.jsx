d44189f3ca8d82881accdda619d1303c50ca611e
Natkrita26 <natkrita.son@dome.tu.ac.th>
2025-03-04 02:01:55 +0700

    .

---
+++ client/src/components/ProductContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+export const ProductContext = createContext();
+
+export function ProductProvider({ children }) {
+  const [products, setProducts] = useState([]);
+  return (
+    <ProductContext.Provider value={{ products, setProducts }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
